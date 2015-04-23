dd41b74329d917d1a21a7c42b1a7356aba9e0236
Lance Ennen <byennen@gmail.com>
2015-04-23 10:11:16 -0400

    Initial Commit

---
+++ Family Perfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Family Perfect
+//
+//  Created by Lance Ennen on 4/23/15.
+//  Copyright (c) 2015 Lance Ennen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
