5b6e309ea3c5f1b28811b44e2f7373b8d8e54b43
Lance Ennen <byennen@gmail.com>
2015-04-28 22:55:58 -0400

    add record button, add recording in progress text, and stop button

--- Family Perfect/ViewController.swift
+++ Family Perfect/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        // TODO: Show text "recording in progress"
+        recordingInProgress.hidden = false
+        // TODO: Record the user's voice
+        println("in recordAudio")
+    }
 
+    @IBAction func stopRecording(sender: UIButton) {
+        recordingInProgress.hidden = true
+    }
 }
 
