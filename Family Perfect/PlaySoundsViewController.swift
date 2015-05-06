fa9061facc702819bc271d3cccea54bd1b91b163
Lance Ennen <byennen@gmail.com>
2015-05-05 21:44:31 -0400

    setup audioPlayer

--- Family Perfect/PlaySoundsViewController.swift
+++ Family Perfect/PlaySoundsViewController.swift
@@ -7,32 +7,25 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
+    
+    var audioPlayer:AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            var filePathUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+        } else {
+            println("the filePath is empty")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     @IBAction func playSlowAudio(sender: UIButton) {
         println("play slow audio")
+        audioPlayer.play()
     }
 }
